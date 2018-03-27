24639b9a4f87c921f9a5cc9cc02c4fbc51485184
GabrielDzul <GabrielDzul@Gmail.com>
2018-03-27 17:29:16 -0600

    Agregar la ruta para eliminar un usuario con base en su id

--- public/src/routes/users.php
+++ public/src/routes/users.php
@@ -22,10 +22,11 @@ $app->group('/user', function(){
             $new_user_data['password'] = filter_var($data['password'], FILTER_SANITIZE_STRING);
         
             $mongoConnection = new MongoConnectionMannager();
-            $mongoConnection->addNewUser($new_user_data);
+            $last_inserted_user_id = $mongoConnection->addNewUser($new_user_data);
 
             $status= json_encode(array('status' => 'success',
-            'message' => 'User added Successfully'));
+            'message' => 'User added Successfully',
+            'user_id' => $last_inserted_user_id));
             $response->getBody()->write($status);
             
 
@@ -45,13 +46,37 @@ $app->group('/user', function(){
             $mongoConnection = new MongoConnectionMannager();
             $user = $mongoConnection->getUserById($userId);
 
-            /*$status= json_encode(array('status' => 'success',
-            'message' => 'Data added Successfully'));*/
-            //var_dump($userId);
-            //var_dump($user);
-            print_r ($user);
-            //$response->getBody()->write($user);
+            
+            echo json_encode($user);
+            //$response->getBody()->write("The user is",$user);
             //return $response;
+            
+
+        }catch(Exception $exception){
+            echo json_encode(array('status' => 'error',
+                'message' => $exception->getMessage()));
+        }
+
+    });
+
+    $this->put('/update/{id}', function (Request $request, Response $response, array $args){
+
+        $userId = new MongoDB\BSON\ObjectId($args['id']);
+        try{
+            $data = $request->getParsedBody();
+            $new_user_data=[];
+            $new_user_data['name'] = filter_var($data['name'], FILTER_SANITIZE_STRING);
+            $new_user_data['lastName'] = filter_var($data['lastName'], FILTER_SANITIZE_STRING);
+            $new_user_data['rol'] = filter_var($data['rol'], FILTER_SANITIZE_STRING);
+            $new_user_data['nick'] = filter_var($data['nick'], FILTER_SANITIZE_STRING);
+            $new_user_data['password'] = filter_var($data['password'], FILTER_SANITIZE_STRING);
+        
+            $mongoConnection = new MongoConnectionMannager();
+            $mongoConnection->updateUser($userId, $new_user_data);
+
+            $status= json_encode(array('status' => 'success',
+            'message' => 'User updated Successfully'));
+            $response->getBody()->write($status);
             
 
         }catch(Exception $exception){
@@ -64,5 +89,7 @@ $app->group('/user', function(){
     $this->get('/mira', function (Request $request, Response $response){
         echo getcwd();
     });
+
+    
 });
 ?>
